d65be164149ea5ef8e6415294740df6a7acea6b3
Paulina Paluch <paulina.paluch1@pollub.edu.pl>
2020-11-06 19:14:33 +0100

    init Roomie Mobile app

---
+++ app/src/main/java/com/pm/roomie/roomie/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pm.roomie.roomie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.osk.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
