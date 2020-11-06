8db347d69ec2556fc3a6968709be01f97054a5cf
Paulina Paluch <paulina.paluch1@pollub.edu.pl>
2020-11-06 20:01:51 +0100

    add files to communication remote

---
+++ app/src/main/java/com/pm/roomie/roomie/remote/ApiUtils.java
@@ -0,0 +1,12 @@
+package com.pm.roomie.roomie.remote;
+
+public class ApiUtils {
+
+    public static final String BASE_URL="http://192.168.43.115:7070/mobile/";
+    //   public static final String BASE_URL="http://192.168.1.11:7070/mobile/";
+    //   public static final String BASE_URL="http://192.168.1.11:7070/mobile/";
+
+    public static UserService getUserService(){
+        return RetrofitClient.getClient(BASE_URL).create(UserService.class);
+    }
+}
---
+++ app/src/main/java/com/pm/roomie/roomie/remote/UserService.java
@@ -0,0 +1,18 @@
+package com.pm.roomie.roomie.remote;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.HTTP;
+import retrofit2.http.Path;
+public interface UserService {
+
+//    @GET("login/{username}/{password}")
+//    Call<Instructor> login(@Path("username") String username,
+//                           @Path("password") String password);
+//
+//    @HTTP(method = "POST", path = "send/{instructor}", hasBody = true)
+//    Call<Message> sendCoordinates(@Body ArrayList<LocationToSend> coordinates,
+//                                  @Path("instructor") Integer currentLoggedInstructorId);
+//    @GET("timetable/{id}")
+//    Call<ArrayList<TimetableJson>> getTodayTimetable(@Path("id") Integer instructorId);
+}
